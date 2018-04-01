dfc69fc956fd807a5873045ee52c7d8020262d1e
ElaineChoo <elainechoo85@gmail.com>
2018-04-01 19:18:18 +0800

    new user done

--- controllers/user.js
+++ controllers/user.js
@@ -13,9 +13,74 @@
  * Controller logic
  * ===========================================
  */
+const newForm = (request, response) => {
+    response.render('user/new');
+};
+
+const create = (allModels) => {
+    return (request, response) => {
+        //create new user entry in database
+        allModels.user.create(request.body, (error, queryResult) => {
+
+            if (error) {
+                console.error('error ??? ', error);
+            }
+
+            if (queryResult.rowCount >= 1) {
+                console.log('User created successfully');
+
+                // drop cookies to indicate user's logged in status and username
+                response.cookie('loggedIn', true);
+                response.cookie('username', request.body.name);
+            } else {
+                console.log('User could not be created');
+                response.sendStatus(500);
+            }
+
+            //redirect to homepage after creation
+            response.redirect('/');
+        });
+    };
+};
+
+const logout = (request, response) => {
+    response.clearCookie('loggedIn');
+    response.redirect(301, '/');
+};
+
+const loginForm = (request, response) => {
+
+    if (request.cookies['loggedIn'] === 'true') {
+        response.redirect('/');
+    } else {
+        response.render('user/login');
+    }
+};
+
+const login = (db) => {
+    return (request, response) => {
+        allModels.user.create(request.body, (request, response) => {
+            if (queryResult) {
+                let userName = request.body.name;
+                response.cookie('loggedIn');
+                response.cookie('username', username);
+                response.redirect('/');
+            } else {
+                response.redirect('/users/login');
+            }
+        });
+    };
+};
 
 /**
  * ===========================================
  * Export controller functions as a module
  * ===========================================
  */
+module.exports = {
+    newForm,
+    create,
+    logout,
+    loginForm,
+    login
+};
--- routes.js
+++ routes.js
@@ -7,3 +7,34 @@
  * Export as a function using `module.exports`,
  * to be imported (using `require(...)`) in `index.js`.
  */
+
+const pokemons = require('./controllers/pokemon');
+const users = require('./controllers/user');
+
+module.exports = (app, db) => {
+    /*
+     *  =========================================
+     *  Users
+     *  =========================================
+     */
+    // CRUD users
+    app.get('/users/new', users.newForm);
+    app.post('/users', users.create(db));
+
+    // Authentication
+    // app.post('/users/logout', users.logout);
+    // app.get('/users/login', users.loginForm);
+    // app.post('/users/login', users.login);
+
+    /*
+     *  =========================================
+     *  Pokemons
+     *  =========================================
+     */
+    // CRUD pokemons
+    // app.get('/pokemons/:id/edit', pokemons.updateForm(db));
+    // app.post('/pokemons/:id/edit', pokemons.update(db));
+    // app.get('/pokemons/new', pokemons.createForm);
+    // app.post('/pokemons', pokemons.create(db));
+    // app.get('/pokemons/:id', pokemons.get(db));
+};
