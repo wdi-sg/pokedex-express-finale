ee117d80126fec0237855e9cec3d1ddbff5bc51c
Edmund <edmundcjj@gmail.com>
2018-04-01 23:20:11 +0800

    Basic CRUD web app completed without further

--- db.js
+++ db.js
@@ -7,3 +7,31 @@
  *
  * Export the pool and models as a module using `module.exports`.
  */
+
+// Import models and `pg` package
+const pg = require('pg');
+const pokemon = require('./models/pokemon');
+const user = require('./models/user');
+
+// Initialise configuration object with database credentials.
+const configs = {
+  user: 'postgres',
+  host: '127.0.0.1',
+  database: 'pokemons_development',
+  port: 5432,
+  password: 'edmund92chow'
+};
+
+// Initialise the connection pool with config object.
+const pool = new pg.Pool(configs);
+
+pool.on('error', function(err) {
+  console.log('idle client error', err.message, err.stack);
+});
+
+// Export the pool and models as a module using 'module.exports'
+module.exports = {
+  pool: pool,
+  pokemon: pokemon(pool),
+  user: user(pool)
+};
--- routes.js
+++ routes.js
@@ -7,3 +7,35 @@
  * Export as a function using `module.exports`,
  * to be imported (using `require(...)`) in `index.js`.
  */
+
+// Import files from the controller so that the functions can be called
+const pokemons = require('./controllers/pokemon');
+const users = require('./controllers/user');
+
+module.exports = (app, db) => {
+  /*
+   *  =========================================
+   *  Users
+   *  =========================================
+   */
+  // CRUD users
+  app.get('/users/new', users.newForm);
+  app.post('/users', users.create(db));
+
+  // Authentication
+  app.post('/users/logout', users.logout);
+  app.get('/users/login', users.loginForm);
+  app.post('/users/login', users.login(db));
+
+  /*
+   *  =========================================
+   *  Pokemons
+   *  =========================================
+   */
+  // CRUD pokemons
+  app.get('/pokemons/:id/edit', pokemons.updateForm(db));
+  app.put('/pokemons/:id', pokemons.update(db));
+  app.get('/pokemons/new', pokemons.createForm);
+  app.post('/pokemons', pokemons.create(db));
+  app.get('/pokemons/:id', pokemons.get(db));
+};
