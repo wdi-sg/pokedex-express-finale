8418e91fcf5b472a7bba43cbed6a8079320fe10f
zw-zack <zhengwin94@hotmail.com>
2018-04-02 09:03:39 +0800

    incomplete

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,21 +1,89 @@
 /**
- * Pokemon controller functions.
- *
- * Each pokemon-related route in `routes.js` will call
- * one controller function here.
- *
- * Export all functions as a module using `module.exports`,
- * to be imported (using `require(...)`) in `routes.js`.
- */
-
-/**
  * ===========================================
  * Controller logic
  * ===========================================
  */
+ const get = (db) => {
+  return (request, response) => {
+    // use pokemon model method `get` to retrieve pokemon data
+    db.pokemon.get(request.params.id, (error, queryResult) => {
+      if (error) {
+        console.error('error getting pokemon:', error);
+        response.sendStatus(500);
+      } else {
+        response.render('pokemon/pokemon', { pokemon: queryResult.rows[0] });
+      }
+    });
+  };
+};
+
+const updateForm = (db) => {
+  return (request, response) => {
+    // TODO: Add logic here
+    db.pokemon.updateForm(request.params.id, (error, queryResult) =>{
+      if (error) {
+        console.error('query error: ', error.stack);
+      } else {
+        response.render('pokemon/edit', { pokemon: queryResult.rows[0] });
+      }
+    });
+  };
+};
+
+const update = (db) => {
+  return (request, response) => {
+    db.pokemon.update(request.body, (error, queryResult)=>{
+      if (error){
+        console.error('query error: ', error.stack);
+      } else{
+        // response.render('pokemon/edit', { pokemon: queryResult.rows[0] });
+        // console.log(queryResult);
+        // response.send("Pokemon updated!")
+      }
+      response.redirect('/');
+    });
+    // TODO: Add logic here
+  };
+};
+
+const createForm = (request, response) => {
+  response.render('pokemon/new');
+};
+
+const create = (db) => {
+  return (request, response) => {
+    // use pokemon model method `create` to create new pokemon entry in db
+    db.pokemon.create(request.body, (error, queryResult) => {
+      // queryResult of creation is not useful to us, so we ignore it
+      // (console log it to see for yourself)
+      // (you can choose to omit it completely from the function parameters)
+
+      if (error) {
+        console.error('error getting pokemon:', error);
+        response.sendStatus(500);
+      }
+
+      if (queryResult.rowCount >= 1) {
+        console.log('Pokemon created successfully');
+      } else {
+        console.log('Pokemon could not be created');
+      }
+
+      // redirect to home page after creation
+      response.redirect('/');
+    });
+  };
+};
 
 /**
  * ===========================================
  * Export controller functions as a module
  * ===========================================
  */
+ module.exports = {
+  get,
+  updateForm,
+  update,
+  createForm,
+  create
+};
--- models/pokemon.js
+++ models/pokemon.js
@@ -1,16 +1,52 @@
 /**
- * Pokemon model functions.
- *
- * Any time a database SQL query needs to be executed
- * relating to a pokemon (be it C, R, U, or D),
- * one or more of the functions here should be called.
- *
- * Export all functions as a module using `module.exports`,
- * to be imported (using `require(...)`) in `db.js`.
- */
-
-/**
  * ===========================================
  * Export model functions as a module
  * ===========================================
  */
+module.exports = (dbPool) => {
+  // `dbPool` is accessible within this function scope
+  return {
+    updateForm: (id, callback) => {
+      const queryString = 'SELECT * from pokemons WHERE id=$1'
+      const values =[id];
+      dbPool.query(queryString, values, (err, queryResult) =>{
+        callback(err, queryResult);
+      });
+    },
+
+    update: (pokemon, callback) => {
+      const queryString = 'UPDATE pokemons SET name = ($1), num = ($2), img = ($3), weight = ($4), height = ($5) WHERE id = ($6)'
+      const values = [pokemon.name, pokemon.num, pokemon.img, pokemon.weight, pokemon.height, pokemon.id];
+      console.log(pokemon.name)
+      dbPool.query(queryString, values, (err, queryResult) =>{
+        callback(err, queryResult);
+      });
+    },
+
+    create: (pokemon, callback) => {
+      // set up query
+      const queryString = 'INSERT INTO pokemons (name, num, img, weight, height) VALUES ($1, $2, $3, $4, $5)';
+      const values = [
+        pokemon.name,
+        pokemon.num,
+        pokemon.img,
+        pokemon.weight,
+        pokemon.height
+      ];
+
+      // execute query
+      dbPool.query(queryString, values, (err, queryResult) => {
+        // invoke callback function with results after query has executed
+        callback(err, queryResult);
+      });
+    },
+
+    get: (id, callback) => {
+      const values = [id];
+
+      dbPool.query('SELECT * from pokemons WHERE id=$1', values, (error, queryResult) => {
+        callback(error, queryResult);
+      });
+    }
+  };
+};
